923ba411f83958af34b07c900a086edf9d6b21d1
Aesen Vismea <aesen@unascribed.com>
2016-09-21 22:05:34 -0400

    Fix all the things

--- src/main/java/io/github/elytra/copo/helper/Numbers.java
+++ src/main/java/io/github/elytra/copo/helper/Numbers.java
@@ -2,22 +2,31 @@ package io.github.elytra.copo.helper;
 
 import java.text.NumberFormat;
 
-import net.minecraft.client.resources.I18n;
+import net.minecraft.util.text.translation.I18n;
 
 public class Numbers {
-	public static final int GIBIBYTE = 1024*1024*1024;
-	public static final int MIBIBYTE = 1024*1024;
-	public static final int KIBIBYTE = 1024;
+	public static final long EXBIBYTE = 1024L*1024L*1024L*1024L*1024L*1024L;
+	public static final long PEBIBYTE = 1024L*1024L*1024L*1024L*1024L;
+	public static final long TEBIBYTE = 1024L*1024L*1024L*1024L;
+	public static final long GIBIBYTE = 1024L*1024L*1024L;
+	public static final long MEBIBYTE = 1024L*1024L;
+	public static final long KIBIBYTE = 1024L;
 	public static String humanReadableBytes(long bytes) {
-		if (bytes == 1) return I18n.format("numbers.correlatedpotentialistics.byte");
-		if (bytes >= GIBIBYTE) {
-			return I18n.format("numbers.correlatedpotentialistics.gibibytes", bytes/GIBIBYTE);
-		} else if (bytes >= MIBIBYTE) {
-			return I18n.format("numbers.correlatedpotentialistics.mibibytes", bytes/MIBIBYTE);
+		if (bytes == 1) return I18n.translateToLocal("numbers.correlatedpotentialistics.byte");
+		if (bytes >= EXBIBYTE) {
+			return I18n.translateToLocalFormatted("numbers.correlatedpotentialistics.exbibytes", bytes/EXBIBYTE);
+		} else if (bytes >= PEBIBYTE) {
+			return I18n.translateToLocalFormatted("numbers.correlatedpotentialistics.pebibytes", bytes/PEBIBYTE);
+		} else if (bytes >= TEBIBYTE) {
+			return I18n.translateToLocalFormatted("numbers.correlatedpotentialistics.tebibytes", bytes/TEBIBYTE);
+		} else if (bytes >= GIBIBYTE) {
+			return I18n.translateToLocalFormatted("numbers.correlatedpotentialistics.gibibytes", bytes/GIBIBYTE);
+		} else if (bytes >= MEBIBYTE) {
+			return I18n.translateToLocalFormatted("numbers.correlatedpotentialistics.mebibytes", bytes/MEBIBYTE);
 		} else if (bytes >= KIBIBYTE) {
-			return I18n.format("numbers.correlatedpotentialistics.kibibytes", bytes/KIBIBYTE);
+			return I18n.translateToLocalFormatted("numbers.correlatedpotentialistics.kibibytes", bytes/KIBIBYTE);
 		}
-		return I18n.format("numbers.correlatedpotentialistics.bytes", bytes);
+		return I18n.translateToLocalFormatted("numbers.correlatedpotentialistics.bytes", bytes);
 	}
 
 	public static final int GIGA = 1_000_000_000;
@@ -33,12 +42,12 @@ public class Numbers {
 			formatter.setMaximumFractionDigits(1);
 		}
 		if (count >= GIGA) {
-			return I18n.format("numbers.correlatedpotentialistics.giga", formatter.format((double)count/GIGA));
+			return I18n.translateToLocalFormatted("numbers.correlatedpotentialistics.giga", formatter.format((double)count/GIGA));
 		} else if (count >= MEGA) {
-			return I18n.format("numbers.correlatedpotentialistics.mega", formatter.format((double)count/MEGA));
+			return I18n.translateToLocalFormatted("numbers.correlatedpotentialistics.mega", formatter.format((double)count/MEGA));
 		} else if (count >= 10_000) {
-			return I18n.format("numbers.correlatedpotentialistics.kilo", formatter.format((double)count/KILO));
+			return I18n.translateToLocalFormatted("numbers.correlatedpotentialistics.kilo", formatter.format((double)count/KILO));
 		}
-		return I18n.format("numbers.correlatedpotentialistics.normal", count);
+		return I18n.translateToLocalFormatted("numbers.correlatedpotentialistics.normal", count);
 	}
 }
